3e0b3ead9ab85c1692c18266a299d30b17609d15
Mohameddev73 <takhlitasimo@gmail.com>
2025-03-30 18:50:39 +0200

    resoudre probleme for navigation

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,6 +1,23 @@
+// navigation/AppNavigator.js
 import React from 'react';
-import AppNavigator from './navigation/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import AddCatScreen from '../screens/AddCatScreen';
+import CatDetailScreen from '../screens/CatDetailScreen';
 
-export default function App() {
-  return <AppNavigator />;
-}
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="AddCat" component={AddCatScreen} />
+        <Stack.Screen name="CatDetail" component={CatDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
